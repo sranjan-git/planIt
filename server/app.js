2f6256e1d7781e86c97f24e33a67dc63e68b354f
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2024-09-23 03:19:26 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:3001/",
+    origin: "http://localhost:3000",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
